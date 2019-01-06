c1ecb486ce7ea004931f44c24cfb666166d1a7c9
Matti Viljanen <matti.viljanen@kapsi.fi>
2019-01-06 11:29:12 +0200

    Update translations, create Finnish translation

--- harbour-batterybuddy.pro
+++ harbour-batterybuddy.pro
@@ -53,7 +53,7 @@ CONFIG += sailfishapp_i18n
 # planning to localize your app, remember to comment out the
 # following TRANSLATIONS line. And also do not forget to
 # modify the localized app name in the the .desktop file.
-TRANSLATIONS += translations/harbour-batterybuddy-de.ts
+TRANSLATIONS += translations/*.ts
 
 HEADERS += \
     src/battery.h \
--- translations/harbour-batterybuddy-fi.ts
+++ translations/harbour-batterybuddy-fi.ts
@@ -1,211 +1,211 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="fi_FI">
 <context>
     <name>AboutPage</name>
     <message>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisenssi</translation>
     </message>
     <message>
         <source>Code and feedback</source>
-        <translation type="unfinished"></translation>
+        <translation>Koodi ja palaute</translation>
     </message>
     <message>
         <source>Battery Buddy</source>
-        <translation type="unfinished"></translation>
+        <translation>Battery Buddy</translation>
     </message>
     <message>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekijä</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Battery Buddy</source>
-        <translation type="unfinished"></translation>
+        <translation>Battery Buddy</translation>
     </message>
 </context>
 <context>
     <name>InfoPage</name>
     <message>
         <source>What is Battery Buddy?</source>
-        <translation type="unfinished"></translation>
+        <translation>Mikä on Battery Buddy?</translation>
     </message>
     <message>
         <source>Battery Buddy is a small and simple utility designed to improve the lifetime of the battery.</source>
-        <translation type="unfinished"></translation>
+        <translation>Battery Buddy on pieni ja yksinkertainen sovellus, jonka tarkoituksena on pidentää akun elinikää.</translation>
     </message>
     <message>
         <source>It simply monitors the battery level and if it has been charged above the limit, it plays a sound and displays a notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se yksinkertaisesti tarkkailee akun varausta, ja jos akku on latautunut yli ylärajan, sovellus toistaa merkkiäänen ja näyttää ilmoituksen.</translation>
     </message>
     <message>
         <source>A different sound is played and notification send when the battery has been discharged under the limit.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eri äänimerkki toistetaan ja ilmoitus näytetään, jos akku on tyhjentynyt alle halutun raja-arvon.</translation>
     </message>
     <message>
         <source>Why does it matter?</source>
-        <translation type="unfinished"></translation>
+        <translation>Mikä merkitys sillä on?</translation>
     </message>
     <message>
         <source>Lithium-based batteries do not last forever. Every charge and discharge wear out the battery a small amount. This is normal and currently unavoidable.</source>
-        <translation type="unfinished"></translation>
+        <translation>Litium-akut eivät kestä käytössä ikuisesti. Jokainen lataus ja tyhjentyminen kuluttavat akkua. Tämä on normaalia ja tällä hetkellä väistämätöntä.</translation>
     </message>
     <message>
         <source>A full charge from 0% to 100% stresses the battery more than, say, charging from 25% to 75%.</source>
-        <translation type="unfinished"></translation>
+        <translation>Akun lataaminen 0%:sta 100%:iin rasittaa akkua enemmän kuin lataamiinen vaikkapa 25%:sta 75%:iin.</translation>
     </message>
     <message>
         <source>Charging and discharging the battery lowers the capacity over time. Small discharge wears out the capacity slower than a deep discharge and a full recharge.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lataaminen ja purkaminen laskee akun kapasiteettia ajan mittaan. Akun osittainen purkautuminen ja osittainen lataaminen pienentävät kapasiteettia hitaammin, kuin täysin tyhjäksi päästäminen ja täyteen asti lataaminen.</translation>
     </message>
     <message>
         <source>Is it noticable?</source>
-        <translation type="unfinished"></translation>
+        <translation>Huomaako eron käytännössä?</translation>
     </message>
     <message>
         <source>Yes, it is. If you always let the battery drain down to 0% and then charge it to 100%, your battery may not see 500 charge cycles.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kyllä. Jos akun antaa aina tyhjetä 0%:iin asti ja sen lataa aina 100% täyteen, akku saattaa olla 500 latauskerran jälkeen käyttökelvoton.</translation>
     </message>
     <message>
         <source>If you only let the battery charge go down to 50% and then charge it to 70%, it could last several thousands of cycles!</source>
-        <translation type="unfinished"></translation>
+        <translation>Jos akkua käyttää ja lataa siten, että varaus vaihtelee 50% ja 70% välillä, se saattaa kestää useita tuhansia latauskertoja!</translation>
     </message>
     <message>
         <source>More info</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisätietoja</translation>
     </message>
     <message>
         <source>Please keep in mind that the numbers are very coarse estimates. There are also other variables that affect the life cycle, such as charge current and battery temperature.</source>
-        <translation type="unfinished"></translation>
+        <translation>Huomaa, että esitetyt numerot ovat erittäin karkeita arvioita. Akun elinikään ja suorituskykyyn vaikuttaa moni muukin asia, esimerkiksi latausvirran suuruus sekä akun käyttölämpötila.</translation>
     </message>
     <message>
         <source>Why should I care?</source>
-        <translation type="unfinished"></translation>
+        <translation>Miksi välittäisin?</translation>
     </message>
     <message>
         <source>Every battery produced uses a lot of energy and at least some non-renewable resources.</source>
-        <translation type="unfinished"></translation>
+        <translation>Jokainen valmistettu akku kuluttaa runsaasti energiaa ja ainakin vähän uusiutumattomia luonnonvaroja.</translation>
     </message>
     <message>
         <source>By taking care of your devices battery, you don&apos;t have to buy a new battery so soon.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pitämällä huolta laitteesi akusta, sinun ei tarvitse kuluttaa rahaa uuden akun hankintaan niin pian.</translation>
     </message>
     <message>
         <source>Even better; in some devices the battery is very difficult to replace without braking the device, which means you don&apos;t have to pay for the service, either.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mikä parempaa, joidenkin laitteiden kohdalla akkua on lähes mahdotonta vaihtaa särkemättä laitetta, mikä tarkoittaa sitä, että et myöskään joudu maksamaan akun vaihtotyöstä.</translation>
     </message>
     <message>
         <source>What else can I do?</source>
-        <translation type="unfinished"></translation>
+        <translation>Mitä muuta voin tehdä?</translation>
     </message>
     <message>
         <source>Uninstall battery-hungry applications you don&apos;t use. Reboot your device every now and then in order to keep it running flawlessly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Poista akkua paljon kuluttavat ohjelmat, joita et käytä. Uudelleenkäynnistä puhelimesi aina silloin tällöin, jotta se toimisi muutenkin moitteettomasti.</translation>
     </message>
     <message>
         <source>Use flight mode overnight whenever possible in order to cut power usage down. Powering the device off and on again may use a lot more battery than you think!</source>
-        <translation type="unfinished"></translation>
+        <translation>Käytä lentotilaa yön ajan aina, kun se on mahdollista, jotta laite kuluttaisi vähiten virtaa. Laitteen sammuttaminen ja uudelleenkäynnistäminen voi kuluttaa virtaa enemmän kuin arvaatkaan!</translation>
     </message>
     <message>
         <source>Don&apos;t use your phone in hot or cold environments. In the winter keep the phone in your pocket near your body (but take note of the moisture) and use a handsfree device. Extreme temperatures are bad for the battery.</source>
-        <translation type="unfinished"></translation>
+        <translation>Älä käytä puhelintasi kylmässä tai kuumassa ympäristössä. Pidä puhelinta talvella taskussa lähellä kehoa (mutta tarkkaile kosteutta) ja käytä handsfreetä. Korkeat ja matalat käyttölämpötilat eivät ole akulle hyväksi.</translation>
     </message>
     <message>
         <source>I want to know more!</source>
-        <translation type="unfinished"></translation>
+        <translation>Haluan tietää enemän!</translation>
     </message>
     <message>
         <source>Great! There are a lot of articles to read and papers to study, so click the link to begin!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hienoa! Arikkeleita ja tutkimustietoa on vaikka millä mitalla, joten alkuun pääset helpoimmin klikkaamalla linkistä!</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>Battery Buddy</source>
-        <translation type="unfinished"></translation>
+        <translation>Battery Buddy</translation>
     </message>
     <message>
         <source>Click the buttons to test the sound and notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>Painikkeista voit testata hälytysäänet ja ilmoituksen.</translation>
     </message>
     <message>
         <source>Battery status</source>
-        <translation type="unfinished"></translation>
+        <translation>Akun tila</translation>
     </message>
     <message>
         <source>charging</source>
-        <translation type="unfinished"></translation>
+        <translation>latauksessa</translation>
     </message>
     <message>
         <source>discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>purkautuu</translation>
     </message>
     <message>
         <source>Charge level</source>
-        <translation type="unfinished"></translation>
+        <translation>Varaustaso</translation>
     </message>
     <message>
         <source>Alert levels</source>
-        <translation type="unfinished"></translation>
+        <translation>Hälytysrajat</translation>
     </message>
     <message>
         <source>Charging limit</source>
-        <translation type="unfinished"></translation>
+        <translation>Latausraja</translation>
     </message>
     <message>
         <source>Discharging limit</source>
-        <translation type="unfinished"></translation>
+        <translation>Purkautumisraja</translation>
     </message>
     <message>
         <source>Alert tests</source>
-        <translation type="unfinished"></translation>
+        <translation>Hälytystestit</translation>
     </message>
     <message>
         <source>Please connect the charger.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ole hyvä ja kytke laturi.</translation>
     </message>
     <message>
         <source>Please disconnect the charger.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ole hyvä ja irrota laturi.</translation>
     </message>
     <message>
         <source>Battery charge</source>
         <comment>Battery charge 20%</comment>
-        <translation type="unfinished"></translation>
+        <translation>Akun varaus</translation>
     </message>
     <message>
         <source>About</source>
         <comment>About this application</comment>
-        <translation type="unfinished"></translation>
+        <translation>Tietoja</translation>
     </message>
     <message>
         <source>Background</source>
         <comment>More to read, background information...</comment>
-        <translation type="unfinished"></translation>
+        <translation>Taustatietoa</translation>
     </message>
     <message>
         <source>Please leave Battery Buddy running in the background in order to receive alerts.</source>
-        <translation type="unfinished"></translation>
+        <translation>Jätä Battery Buddy taustalle käyntiin, jotta hälytykset toimivat.</translation>
     </message>
     <message>
         <source>Set the maximum and minimum target charge levels.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseta haluamasi ylä- ja alaraja latauksen tavoitearvoiksi.</translation>
     </message>
     <message>
         <source>This is a test.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tämä on testi.</translation>
     </message>
     <message>
         <source>Discharged</source>
-        <translation type="unfinished"></translation>
+        <translation>Tyhjä</translation>
     </message>
     <message>
         <source>Charged</source>
-        <translation type="unfinished"></translation>
+        <translation>Täysi</translation>
     </message>
 </context>
 </TS>
