20c375f8bd8f6c331758a27009c7e3431fa71983
dryo <github.com@deryo.de>
2020-11-02 20:28:30 +0100

    Added german translation

---
+++ application/translations/harbour-batterybuddy_de.ts
@@ -0,0 +1,330 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Battery Buddy is a small and simple utility designed to improve the lifetime of the battery.</source>
+        <translation>Battery Buddy ist ein kleines und einfaches Dienstprogramm, das dazu gedacht ist, die Lebensdauer des Akkus zu verlängern.</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <source>View License</source>
+        <translation>Lizenz anzeigen</translation>
+    </message>
+    <message>
+        <source>Battery Buddy is open source software licensed under the terms of the GNU General Public License.</source>
+        <translation>Battery Buddy ist ein quelloffenes Programm und steht unter der GNU-General-Public-Lizenz.</translation>
+    </message>
+    <message>
+        <source>Contributions and translations are always appreciated!</source>
+        <translation>Mitarbeit und Übersetzungen sind sehr willkommen!</translation>
+    </message>
+    <message>
+        <source>If you like my work and would like to support me, you can buy me a coffee!</source>
+        <translation>Falls dir die Software gefällt und du mich unterstützen möchtest, kannst du mir einen Kaffee spendieren!</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Übersetzungen</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Charging</source>
+        <translatorcomment>Ggf. &quot;Wird geladen&quot;</translatorcomment>
+        <translation>Lädt</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translatorcomment>ggf. &quot;Wird nicht geladen&quot;</translatorcomment>
+        <translation>Lädt nicht</translation>
+    </message>
+    <message>
+        <source>Connect charger</source>
+        <translation>Ladegerät anschließen</translation>
+    </message>
+    <message>
+        <source>Disconnect charger</source>
+        <translation>Ladegerät trennen</translation>
+    </message>
+    <message>
+        <source>Battery Buddy</source>
+        <translation>Battery Buddy</translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <source>What is Battery Buddy?</source>
+        <translation>Was ist Battery Buddy?</translation>
+    </message>
+    <message>
+        <source>Battery Buddy is a small and simple utility designed to improve the lifetime of the battery.</source>
+        <translation>Battery Buddy ist ein kleines und einfaches Dienstprogramm, das dazu gedacht ist, die Lebensdauer des Akkus zu verlängern.</translation>
+    </message>
+    <message>
+        <source>It simply monitors the battery level and if it has been charged above the limit, it plays a sound and displays a notification.</source>
+        <translation>Es überwacht den Akkufüllstand und wenn er über einer gewissen Grenze ist, erklingt ein Ton und eine Hinweis wird eingeblendet.</translation>
+    </message>
+    <message>
+        <source>A different sound is played and notification send when the battery has been discharged under the limit.</source>
+        <translatorcomment>Klingt hochgestochen, mir ist aber nix besseres eingefallen :D</translatorcomment>
+        <translation>Ein anderer Klang ertönt und ein Hinweis wird eingeblendet, wenn der Akku bis unter eine weitere Grenze entladen wird.</translation>
+    </message>
+    <message>
+        <source>Why does it matter?</source>
+        <translation>Warum ist das wichtig?</translation>
+    </message>
+    <message>
+        <source>Lithium-based batteries do not last forever. Every charge and discharge wear out the battery a small amount. This is normal and currently unavoidable.</source>
+        <translation>Lithium-basierte Akkus halten nicht ewig. Jeder Lade- und Entladevorgang schädigt den Akku minimal. Das ist normal und momentan unvermeidbar.</translation>
+    </message>
+    <message>
+        <source>A full charge from 0% to 100% stresses the battery more than, say, charging from 25% to 75%.</source>
+        <translation>Das komplette Laden und Entladen von 0% bis 100% beansprucht den Akku wesentlich stärker, als z.B. von 25% bis 75%.</translation>
+    </message>
+    <message>
+        <source>Charging and discharging the battery lowers the capacity over time. Small discharge wears out the capacity slower than a deep discharge and a full recharge.</source>
+        <translation>Durch das Laden und Entladen wird die Kapazität des Akkus mit der Zeit geringer. Kleine Ladebereiche lassen den Akku langsamer altern als das komplette Entladen und Aufladen.</translation>
+    </message>
+    <message>
+        <source>Is it noticable?</source>
+        <translation>Merkt man das überhaupt?</translation>
+    </message>
+    <message>
+        <source>Yes, it is. If you always let the battery drain down to 0% and then charge it to 100%, your battery may not see 500 charge cycles.</source>
+        <translation>Ja, allerdings. Wenn du den Akku jedes Mal auf 0% entlädtst und anschließend wieder auf 100% auflädtst, wird er vermutlich keine 500 Ladezyklen durchhalten.</translation>
+    </message>
+    <message>
+        <source>If you only let the battery charge go down to 50% and then charge it to 70%, it could last several thousands of cycles!</source>
+        <translation>Wenn du den Akkustand niemals unter 50% gehen lässt und dann nur bis 70% auflädtst, kann der Akku mehrere 1000 Ladezyklen halten!</translation>
+    </message>
+    <message>
+        <source>More info</source>
+        <translation>Mehr Informationen</translation>
+    </message>
+    <message>
+        <source>Please keep in mind that the numbers are very coarse estimates. There are also other variables that affect the life cycle, such as charge current and battery temperature.</source>
+        <translation>Bitte denke daran, dass diese Zahlen nur zur Verdeutlichung sind. Es gibt weitere Faktoren, die einen Einfluss auf die Liebensdauer haben, wie z.B. der Ladestrom oder die Akkutemperatur.</translation>
+    </message>
+    <message>
+        <source>Why should I care?</source>
+        <translation>Warum sollte mich das interessieren?</translation>
+    </message>
+    <message>
+        <source>Every battery produced uses a lot of energy and at least some non-renewable resources.</source>
+        <translation>Die Herstellung jedes Akkus benötigt eine Menge Energie und auch ein paar nicht erneuerbare Ressourcen.</translation>
+    </message>
+    <message>
+        <source>By taking care of your devices battery, you don&apos;t have to buy a new battery so soon.</source>
+        <translation>Wenn du den Akku deines Geräts pflegst, brauchst du nicht so schnell einen neuen.</translation>
+    </message>
+    <message>
+        <source>Even better; in some devices the battery is very difficult to replace without braking the device, which means you don&apos;t have to pay for the service, either.</source>
+        <translation>Noch besser; Bei manchen Geräten ist es sehr schwierig, den Akku zu tauschen, ohne das Gerät zu beschädigen. Das heißt, dass du die Kosten eines Austausches ggf. sparen kannst.</translation>
+    </message>
+    <message>
+        <source>What else can I do?</source>
+        <translation>Was kann ich noch machen?</translation>
+    </message>
+    <message>
+        <source>Uninstall battery-hungry applications you don&apos;t use. Reboot your device every now and then in order to keep it running flawlessly.</source>
+        <translation>Deinstalliere energiehungrige Programme, die du nicht benötigst. Starte dein Gerät hin und wieder neu, damit es fehlerlos läuft.</translation>
+    </message>
+    <message>
+        <source>Use flight mode overnight whenever possible in order to cut power usage down. Powering the device off and on again may use a lot more battery than you think!</source>
+        <translation>Benutze den Flugmodus z.B. über Nacht, anstatt das Gerät auszuschalten. Das Aus- und Einschalten kann deutlich mehr Energie verbrauchen, als du denkst!</translation>
+    </message>
+    <message>
+        <source>Don&apos;t use your phone in hot or cold environments. In the winter keep the phone in your pocket near your body (but take note of the moisture) and use a handsfree device. Extreme temperatures are bad for the battery.</source>
+        <translation>Benutze dein Telefon möglichst nicht in heißer oder kalter Umgebung. Stecke das Telefon im Winter in eine Innentasche, die nah an deinem Körper ist (aber achte auf Feuchtigkeit) und benutze einen Kopfhörer. Extreme Temperaturen sind schlecht für den Akku.</translation>
+    </message>
+    <message>
+        <source>I want to know more!</source>
+        <translation>Ich möchte mehr wissen!</translation>
+    </message>
+    <message>
+        <source>Great! There are a lot of articles to read and papers to study, so click the link to begin!</source>
+        <translation>Großartig! Es gibt eine Menge Artikel zu lesen und Veröffentlichungen, die man studieren kann. Also klicke auf den Link, um zu beginnen!</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Battery Buddy</source>
+        <translation>Battery Buddy</translation>
+    </message>
+    <message>
+        <source>Battery status</source>
+        <translation>Akkuzustand</translation>
+    </message>
+    <message>
+        <source>Background</source>
+        <comment>More to read, background information...</comment>
+        <translatorcomment>&quot;Hintergrund&quot; allein finde ich genau so doppeldeutig, wie im Englischen. Man könnte denken, es geht um den Hintergrunddienst.</translatorcomment>
+        <translation>Hintergrundinformationen</translation>
+    </message>
+    <message>
+        <source>yes</source>
+        <translation>ja</translation>
+    </message>
+    <message>
+        <source>no</source>
+        <translation>nein</translation>
+    </message>
+    <message>
+        <source>Charge:</source>
+        <translation>Füllstand:</translation>
+    </message>
+    <message>
+        <source>State:</source>
+        <translation>Status:</translation>
+    </message>
+    <message>
+        <source>idle</source>
+        <comment>Charger plugged in, not using nor charging battery</comment>
+        <translation>nicht in Benutzung</translation>
+    </message>
+    <message>
+        <source>discharging</source>
+        <comment>Charger not plugged in, battery discharging</comment>
+        <translation>wird entladen</translation>
+    </message>
+    <message>
+        <source>charging</source>
+        <comment>Charger plugged in and battery charging</comment>
+        <translation>wird geladen</translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <comment>Battery not detected, or faulty, or something</comment>
+        <translation>unbekannt</translation>
+    </message>
+    <message>
+        <source>Charger connected:</source>
+        <translation>Ladegerät angeschlossen:</translation>
+    </message>
+    <message>
+        <source>full</source>
+        <comment>Battery fully charged</comment>
+        <translation>voll</translation>
+    </message>
+    <message>
+        <source>empty</source>
+        <comment>Battery fully depleted</comment>
+        <translation>leer</translation>
+    </message>
+    <message>
+        <source>You can close Battery Buddy when you are done, notifications and charger control will continue working in the background.</source>
+        <translation>Du kannst Battery Buddy schließen wenn du alles erledigt hast. Hinweise und die Ladesteuerung laufen im Hintergrund weiter.</translation>
+    </message>
+    <message>
+        <source>Background service</source>
+        <translation>Hintergrunddienst</translation>
+    </message>
+    <message>
+        <source>If notifications misbehave or there are problems with charger control, restarting the background service should help.</source>
+        <translation>Falls die Hinweise verrückt spielen oder es Probleme mit der Ladesteuerung gibt, sollte ein Neustart des Hintergrunddienstes Abhilfe schaffen.</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Stopp</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Battery charge %1%</source>
+        <translation>Akkustand %1%</translation>
+    </message>
+    <message>
+        <source>Please connect the charger.</source>
+        <translation>Bitte das Ladegerät anschließen.</translation>
+    </message>
+    <message>
+        <source>Please disconnect the charger.</source>
+        <translation>Bitte vom Ladegerät trennen.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Resume charging limit</source>
+        <translation>Laden wiederaufnehmen bei</translation>
+    </message>
+    <message>
+        <source>Charging settings</source>
+        <translation>Laden</translation>
+    </message>
+    <message>
+        <source>Notification settings</source>
+        <translation>Hinweise</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <comment>About this application</comment>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Pause charging limit</source>
+        <translation>Laden stoppen bei</translation>
+    </message>
+    <message>
+        <source>Display visual and audible notifications about reached battery charge levels, when the battery charge is below or above desired percentage.</source>
+        <translation>Visuelle und akustische Hinweise beim über- bzw. unterschreiten der eingestellten Grenzen geben.</translation>
+    </message>
+    <message>
+        <source>Battery low notification</source>
+        <translation>Akkustand ist gering</translation>
+    </message>
+    <message>
+        <source>Battery full notification</source>
+        <translation>Akku ist voll</translation>
+    </message>
+    <message>
+        <source>Automatic charging control</source>
+        <translation>Automatische Laderegelung</translation>
+    </message>
+    <message>
+        <source>This option disables charging automatically when the battery has charged above the pausing percentage and enables it again when the battery has depleted below the resuming percentage.</source>
+        <translation>Mit dieser Option kann mein einstellen, dass das Gerät automatisch aufhört zu laden, wenn der Akku über dem festgelegten Grenzwert liegt. Das Laden wird fortgesetzt, wenn der Füllstand unter den zweiten Grenzwert fällt.</translation>
+    </message>
+    <message>
+        <source>Background service</source>
+        <translation>Hintergrunddienst</translation>
+    </message>
+    <message>
+        <source>Start background service at startup</source>
+        <translation>Starte den Hintergrunddienst beim Einschalten</translation>
+    </message>
+    <message>
+        <source>Show high charge notification</source>
+        <translation>Zeige Hinweis, wenn der Akku voll ist</translation>
+    </message>
+    <message>
+        <source>Show low charge notification</source>
+        <translation>Zeige Hinweis, wenn der Akku leer ist</translation>
+    </message>
+    <message>
+        <source>Battery high notification interval</source>
+        <translation>Intervall für Hinweis &quot;battery voll&quot;</translation>
+    </message>
+    <message>
+        <source>Battery low notification interval</source>
+        <translation>Intervall für Hinweis &quot;battery leer&quot;</translation>
+    </message>
+</context>
+</TS>
