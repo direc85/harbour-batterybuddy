354ffcb295ec330ce4112fc4f8b34a168772cb63
Matti Viljanen <matti.viljanen@kapsi.fi>
2022-08-07 23:16:52 +0300

    Add Jolla Tablet charger control file to permission service

--- application/service/set-write-permissions.sh
+++ application/service/set-write-permissions.sh
@@ -19,4 +19,5 @@
 chmod 666 /sys/class/power_supply/battery/input_suspend 2>/dev/null
 chmod 666 /sys/class/power_supply/battery/charging_enabled 2>/dev/null
 chmod 666 /sys/class/power_supply/usb/charger_disable 2>/dev/null
+chmod 666 /sys/class/power_supply/dollar_cove_battery/enable_charging 2>/dev/null
 exit 0
