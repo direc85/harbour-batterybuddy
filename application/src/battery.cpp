6f24ec2a04fc4970c7fd5bdc5d0a4ba8c53c947b
nephros <sailfish@nephros.org>
2023-02-23 11:16:23 +0100

    [PineTab] add AC charger state file

--- application/src/battery.cpp
+++ application/src/battery.cpp
@@ -72,7 +72,8 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
     filenames.clear();
     filenames << "/sys/class/power_supply/usb/present"
               << "/sys/class/power_supply/dollar_cove_charger/present"
-              << "/sys/class/power_supply/axp20x-usb/present";
+              << "/sys/class/power_supply/axp20x-usb/present"
+              << "/sys/class/power_supply/axp20x-ac/present";
 
     foreach(const QString& file, filenames) {
         if(!chargerConnectedFile && QFile::exists(file)) {
