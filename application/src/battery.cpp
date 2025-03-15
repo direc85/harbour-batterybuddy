beaf6b3d7bbed6364ac62ed8fdf7ce0b90c3ddc7
nephros <nemo@JollaC2>
2025-03-15 13:07:00 +0100

    Remove common file lists from app

--- application/src/battery.cpp
+++ application/src/battery.cpp
@@ -40,13 +40,6 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
 
     logL("Battery charge file: " + (chargeFile ? chargeFile->fileName() : notFound));
 
-    // Number: battery/charging current, e.g. -1450000 (-145mA)
-    const QStringList currentFiles = {
-        "/sys/class/power_supply/battery/current_now",
-        "/sys/class/power_supply/dollar_cove_battery/current_now",
-        "/sys/class/power_supply/axp20x-battery/current_now"
-    };
-
     foreach(const QString& file, currentFiles) {
         if(!currentFile && QFile::exists(file)) {
             currentFile = new QFile(file, this);
@@ -103,13 +96,6 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
 
     logL("AC status file: " + (acConnectedFile ? acConnectedFile->fileName() : notFound));
 
-    // Number: temperature
-    const QStringList tempFiles = {
-        "/sys/class/power_supply/battery/temp",
-        "/sys/class/power_supply/dollar_cove_battery/temp",
-        "/sys/class/power_supply/axp20x-battery/hwmon0/in0_input"
-    };
-
     foreach(const QString& file, tempFiles) {
         if(!temperatureFile && QFile::exists(file)) {
             temperatureFile = new QFile(file, this);
@@ -124,13 +110,6 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
 
     logL("Battery temperature file: " + (temperatureFile ? temperatureFile->fileName() : notFound));
 
-    // String: health state
-    const QStringList healthFiles = {
-        "/sys/class/power_supply/battery/health",
-        "/sys/class/power_supply/dollar_cove_battery/health",
-        "/sys/class/power_supply/axp20x-battery/health"
-    };
-
     foreach(const QString& file, healthFiles) {
         if(!healthFile && QFile::exists(file)) {
             healthFile = new QFile(file, this);
@@ -139,14 +118,6 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
     }
 
     logL("Battery health file: " + (healthFile ? healthFile->fileName() : notFound));
-
-    // Charger control file
-    const QStringList controlFiles = {
-        "/sys/class/power_supply/battery/input_suspend",              // e.g. Sony Xperia XA2
-        "/sys/class/power_supply/battery/charging_enabled",           // e.g. for Sony Xperia Z3 Compact Tablet
-        "/sys/class/power_supply/usb/charger_disable",                // e.g. for Jolla Phone
-        "/sys/class/power_supply/dollar_cove_battery/enable_charging" // e.g. for Jolla Tablet
-    };
 
     foreach(const QString& file, controlFiles) {
         if(!chargingEnabledFile && QFile::exists(file)) {
