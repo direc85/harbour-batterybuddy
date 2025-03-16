d761ac5ff42baae3aa02435af3c59d7d90906a67
Matti Viljanen <matti.viljanen@kapsi.fi>
2025-03-16 14:11:17 +0200

    Clean up and modernize the script

--- build-rpms.sh
+++ build-rpms.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # For clean build, remove $SHADOW folder
 # To trigger qmake, remove $SHADOW/*/Makefile
@@ -7,108 +8,83 @@
 # Configuration
 ##########
 
-export APP_NAME=$(grep "Name" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(yaml|spec)" -print | head -1) | awk '{print $2}')
-export SFOS_VER=4.4.0.58
+export SFOS_VER=5.0.0.62
+export SDK_DIR="$HOME/SailfishOS"
 
 ##########
 # Paths and versions
 ##########
 
-export ORIG_PATH=$PATH
 export PROJECT=$PWD
-export PRO_FILE=$PROJECT/$APP_NAME.pro
 export SHADOW=$PROJECT/shadow
 export RPM_DEST_DIR=$PROJECT/RPMS
 
-export APP_VER=$(grep "Version" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(yaml|spec)" -print | head -1) | awk '{print $2}')
-export APP_REL=$(grep "Release" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(yaml|spec)" -print | head -1) | awk '{print $2}')
+export APP_NAME=$(grep "Name" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(spec)" -print -quit) | awk '{print $2}')
+export APP_VER=$(grep "Version" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(spec)" -print -quit) | awk '{print $2}')
+export APP_REL=$(grep "Release" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(spec)" -print -quit) | awk '{print $2}')
+
+export PRO_FILE=$PROJECT/$APP_NAME.pro
 
 ##########
 # Build function. Ran once per architecture
 ##########
 
 function build() {
-  if [ ! -d "$HOME/SailfishOS/mersdk/targets/SailfishOS-$SFOS_VER-$ARCH.default" ]
-  then
+  if [ ! -d "$SDK_DIR/mersdk/targets/SailfishOS-$SFOS_VER-$ARCH.default" ]; then
     echo Invalid build target.
-    echo $HOME/SailfishOS/mersdk/targets/SailfishOS-$SFOS_VER-$ARCH.default
+    echo $SDK_DIR/mersdk/targets/SailfishOS-$SFOS_VER-$ARCH.default
     exit 1
   fi
-  export PATH="$HOME/SailfishOS/bin:$HOME/SailfishOS/mersdk/targets/SailfishOS-$SFOS_VER-$ARCH.default/usr/lib/qt5/bin:$HOME/.config/SailfishSDK/libsfdk/build-target-tools/Sailfish SDK Build Engine/SailfishOS-$SFOS_VER-$ARCH.default:$ORIG_PATH"
-  export SFDK_OPTIONS="-c target=SailfishOS-$SFOS_VER-$ARCH"
-  sfdk config --global --push no-fix-version
-  cd $PROJECT
-  if [ ! -d "$SHADOW" ]
-  then
-    mkdir $SHADOW
-  fi
-  cd $SHADOW
-  if [ ! -d "$ARCH" ]
-  then
-    mkdir $ARCH
-  fi
-  cd $ARCH
-  if [ ! -f "Makefile" ]
-  then
-    sfdk qmake $PROJECT -recursive
-  fi
+
+  local PATH="$SDK_DIR/bin:$SDK_DIR/mersdk/targets/SailfishOS-$SFOS_VER-$ARCH.default/usr/lib/qt5/bin:$HOME/.config/SailfishSDK/libsfdk/build-target-tools/Sailfish SDK Build Engine/SailfishOS-$SFOS_VER-$ARCH.default:$PATH"
+
+  pushd $PROJECT
+  [ -d "$SHADOW/$ARCH" ] || mkdir -p "$SHADOW/$ARCH"
+  pushd "$SHADOW/$ARCH"
+
+  sfdk config --session --push no-fix-version
+  sfdk config --session --push target SailfishOS-$SFOS_VER-$ARCH
+  sfdk config --session --push-mask output-prefix
+
+  [ -f "Makefile" ] || sfdk qmake $PROJECT -recursive
   sfdk make
   sfdk package
-#  perl-rename -v 's/\+.{29}//g' RPMS/*.rpm
-  cp -f -v RPMS/*.rpm $RPM_DEST_DIR/
-  cd $PROJECT
+
+  sfdk config --session --drop no-fix-version
+  sfdk config --session --drop target
+  sfdk config --session --drop output-prefix
+
+  mv -f -v RPMS/*.rpm $RPM_DEST_DIR/
+
+  popd
+  popd
 }
 
 ##########
 # Build the packages
 ##########
 
-RPM_NOARCH=0
+[ -d "$RPM_DEST_DIR" ] || mkdir $RPM_DEST_DIR
+
 if [ $(grep "^BuildArch:\W*noarch$" $(find . -regextype egrep -regex "\.\/rpm\/[a-z0-9_-]*\.(yaml|spec)" -print) | wc -l) -gt 0 ]
 then
   RPM_NOARCH=1
-fi
-
-if [ ! -d "$RPM_DEST_DIR" ]
-then
-  mkdir $RPM_DEST_DIR
-fi
-
-if [ $RPM_NOARCH -eq 0 ]
-then
-  export ARCH=aarch64
-  build
-
-  export ARCH=armv7hl
-  build
-fi
-
-# Use i486 for noarch build
-export ARCH=i486
-build
-
-##########
-# Run the validator only once
-##########
-
-if [ $RPM_NOARCH -gt 0 ]
-then
   RPM_ARCH=noarch
 else
-  RPM_ARCH=$ARCH
+  RPM_NOARCH=0
+  RPM_ARCH=aarch64
 fi
 
-export RPM_FILE=$(ls RPMS/*$RPM_ARCH.rpm | head -1)
+for ARCH in i486 armv7hl aarch64; do
+  build
+  # If noarch, only build i486
+  [ $RPM_NOARCH -eq 0 ] || break
+done
 
-if [ -f "$RPM_FILE" ]
-then
-  $HOME/SailfishOS/bin/sfdk engine exec rpmvalidation -t SailfishOS-$SFOS_VER-$ARCH.default $(ls RPMS/*$RPM_ARCH.rpm | head -1)
-  echo
-  echo "Changelog:"
-  echo
-  echo ${APP_VER}-${APP_REL}
-  sed '/^$/Q' rpm/${APP_NAME}.changes | tail +2 | cut -c3-
-  echo
+export RPM_FILE="RPMS/$APP_NAME-$APP_VER-$APP_REL.$RPM_ARCH.rpm"
+
+if [ -f "$RPM_FILE" ]; then
+  "$SDK_DIR/bin/sfdk" engine exec rpmvalidation -t "SailfishOS-$SFOS_VER-$ARCH.default" "$RPM_FILE"
 else
   echo "RPM package not found, not validating."
 fi
