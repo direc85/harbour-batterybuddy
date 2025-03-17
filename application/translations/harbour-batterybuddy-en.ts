510745c287724f6c09f3592af3fc5cdb3a35c2c1
nephros <nemo@JollaC2>
2025-03-17 11:02:08 +0100

    Add English translation file
    it's necessary for numerusform (plurals) to work

---
+++ application/translations/harbour-batterybuddy-en.ts
@@ -0,0 +1,531 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Battery Buddy is a small and simple utility designed to improve the lifetime of the battery.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>View License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery Buddy is open source software licensed under the terms of the GNU General Public License.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contributions and translations are always appreciated!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you like my work and would like to support me, you can buy me a coffee!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contributors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A modified version of %1 by %2 in the emoji pack %3 is used in the logo. License: %4</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>%1:%2</source>
+        <comment>a time duration, hours and minutes</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <source>What is Battery Buddy?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery Buddy is a small and simple utility designed to improve the lifetime of the battery.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>It simply monitors the battery level and if it has been charged above the limit, it plays a sound and displays a notification.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A different sound is played and notification send when the battery has been discharged under the limit.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Why does it matter?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lithium-based batteries do not last forever. Every charge and discharge wear out the battery a small amount. This is normal and currently unavoidable.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A full charge from 0% to 100% stresses the battery more than, say, charging from 25% to 75%.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charging and discharging the battery lowers the capacity over time. Small discharge wears out the capacity slower than a deep discharge and a full recharge.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Is it noticable?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes, it is. If you always let the battery drain down to 0% and then charge it to 100%, your battery may not see 500 charge cycles.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you only let the battery charge go down to 50% and then charge it to 70%, it could last several thousands of cycles!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>More info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please keep in mind that the numbers are very coarse estimates. There are also other variables that affect the life cycle, such as charge current and battery temperature.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Why should I care?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Every battery produced uses a lot of energy and at least some non-renewable resources.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>By taking care of your devices battery, you don&apos;t have to buy a new battery so soon.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Even better; in some devices the battery is very difficult to replace without braking the device, which means you don&apos;t have to pay for the service, either.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>What else can I do?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Uninstall battery-hungry applications you don&apos;t use. Reboot your device every now and then in order to keep it running flawlessly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use flight mode overnight whenever possible in order to cut power usage down. Powering the device off and on again may use a lot more battery than you think!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Don&apos;t use your phone in hot or cold environments. In the winter keep the phone in your pocket near your body (but take note of the moisture) and use a handsfree device. Extreme temperatures are bad for the battery.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I want to know more!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Great! There are a lot of articles to read and papers to study, so click the link to begin!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LogPage</name>
+    <message>
+        <source>View log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quiet</source>
+        <comment>Low log setting</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Verbose</source>
+        <comment>Medium log setting</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <comment>High log setting</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Battery Buddy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Background</source>
+        <comment>More to read, background information...</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>no</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charge:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>State:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>idle</source>
+        <comment>Charger plugged in, not using nor charging battery</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>discharging</source>
+        <comment>Charger not plugged in, battery discharging</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>charging</source>
+        <comment>Charger plugged in and battery charging</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <comment>Battery not detected, or faulty, or something</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charger connected:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>full</source>
+        <comment>Battery fully charged</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>empty</source>
+        <comment>Battery fully depleted</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You can close Battery Buddy when you are done, notifications and charger control will continue working in the background.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Background service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If notifications misbehave or there are problems with charger control, restarting the background service should help.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Good</source>
+        <comment>Battery is OK</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warm</source>
+        <comment>Battery is warm</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overheated</source>
+        <comment>Battery is very hot</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cool</source>
+        <comment>Battery is cool</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cold</source>
+        <comment>Battery is very cold</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Health:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperature:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <comment>About this application</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>View log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>USB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <comment>Charger not detected, or faulty, or something</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>not charging</source>
+        <comment>Charger plugged in, not using nor charging battery</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Time remaining:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n minute(s)</source>
+        <comment>time left</comment>
+        <translation>
+            <numerusform>one minute</numerusform>
+            <numerusform>%n minutes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n hour(s)</source>
+        <comment>time left</comment>
+        <translation>
+            <numerusform>one hour</numerusform>
+            <numerusform>%n hours</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Never</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>30 seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2.5 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>5 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>15 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>30 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1 hour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>8 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>12 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>24 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Battery charge %1%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please connect the charger.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please disconnect the charger.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery health %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery health is not good</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery health is critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Resume charging limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charging settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Notification settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pause charging limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Display visual and audible notifications about reached battery charge levels, when the battery charge is below or above desired percentage.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery low notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery full notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Automatic charging control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This option disables charging automatically when the battery has charged above the pausing percentage and enables it again when the battery has depleted below the resuming percentage.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Background service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start background service at startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery high notification interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery low notification interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Never</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Display visual and audible notifications when the battery status exceeds safe values.&lt;br /&gt;This usually means high (or low) temperature but can include other parameters depending on the hardware.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Health notification interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery health notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Health notification settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Notification threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Maximum Charge Current</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
